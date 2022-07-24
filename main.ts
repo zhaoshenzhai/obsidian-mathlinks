c2a1b6a9146def49c5d075ff23a4a9b07f79e3d6
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-07-25 01:06:14 +0400

    Update links in `backFiles`

--- main.ts
+++ main.ts
@@ -5,206 +5,82 @@ export default class MathLinks extends Plugin {
         const { vault } = this.app;
         const { workspace } = this.app;
         const { metadataCache } = this.app;
-        const { fileManager } = this.app;
 
-        // Command to add mathLink. Only available if there is no mathLink
-        this.addCommand({
-            id: 'add-mathlink',
-            name: 'Add a MathLink to the current file',
-            checkCallback: (checking: boolean) => {
-                const view = workspace.getActiveViewOfType(MarkdownView);
-                if (view) {
-                    let currentFile = workspace.getActiveFile();
-                    let mathLink = getMathLink();
-                    if (mathLink === undefined || mathLink[0] === null) {
-                        if (!checking) {
-                            let editor = view.editor;
-                            let frontMatter = getFrontMatter();
-                            // If no frontMatter
-                            if (frontMatter === undefined) {
-                                // Write frontMatter and focus
-                                editor.replaceRange('---\n', {line: 0, ch: 0})
-                                editor.replaceRange('mathLink: \n', {line: 1, ch: 0})
-                                editor.replaceRange('---\n', {line: 2, ch: 0})
-                                editor.setCursor(1);
-                                editor.focus();
-                            } else {
-                                // Change to edit mode
-                                let currentState = workspace.getLeaf().getViewState();
-                                currentState.state.mode = 'source';
-                                workspace.getLeaf(false).setViewState(currentState);
+        // Update all links in backLinkFiles if file (with mathLink) is changed.
+        // Want to modify it so it runs only if a mathLink is updated/generated.
+        metadataCache.on('changed', async (file: TFile, data: string, cache: CachedMetaData) => {
+            console.log(file.name);
+            let mathLink = await getMathLink(file);
+            if (mathLink != null) {
+                let backLinkFilePaths: string[] = [];
+                Object.keys(metadataCache.resolvedLinks).forEach((key) => {
+                    let links = metadataCache.resolvedLinks[key];
+                    Object.keys(links).forEach((link) => {
+                        if (link === file.path) {
+                            backLinkFilePaths.push(key);
+                        }
+                    });
+                });
 
-                                // If no mathLink
-                                if (mathLink === undefined) {
-                                    // Add mathLink key and focus
-                                    let lineToAppendTo = frontMatter[1].to.line - 1;
-                                    editor.setLine(lineToAppendTo, editor.getLine(lineToAppendTo) + '\nmathLink: ');
-                                    editor.setCursor(lineToAppendTo + 1);
-                                    editor.focus();
-                                } else {
-                                    // Move to mathLink line and focus
-                                    editor.setCursor(mathLink[1], editor.getLine(mathLink[1]));
-                                    editor.focus();
-                                }
+                if (backLinkFilePaths.length != 0) {
+                    backLinkFilePaths.forEach(async (backLinkFilePath) => {
+                        let backLinkFile = vault.getAbstractFileByPath(backLinkFilePath);
+                        if (backLinkFile instanceof TFile) {
+                            let backLinkFileContent = await vault.read(backLinkFile);
+
+                            let left = mathLink[0].replace(/^/, '[').replace(/$/, ']');
+                            let right = file.name.replace(/^/, '(').replace(/$/, ')').replace(/\s/g, '%20');
+                            let newLink = `${left}${right}`
+
+                            let rightFormatted = right.replace(/\./, '\\.').replace(/\(/, '\\(').replace(/\)/, '\\)').replace(/\%/, '\\%');
+                            let mixedLink = new RegExp('\\[((?!\\]\\(|\\]\\]).)*\\]' + rightFormatted, 'g');
+                            let doubleLink = new RegExp(file.name.replace(/^/, '\\[\\[').replace(/\.md$/, '\\]\\]').replace(/\s/, '\\s'), 'g');
+
+                            let modified = backLinkFileContent.replace(mixedLink, newLink).replace(doubleLink, newLink);
+                            if (backLinkFileContent != modified) {
+                                vault.modify(backLinkFile, modified);
                             }
                         }
-                        return true;
-                    }
+                    });
                 }
-                return false;
             }
         });
-
-        // Command to edit mathLink. Only available if mathLink exists
-        this.addCommand({
-            id: 'edit-mathlink',
-            name: 'Edit the MathLink of the current file',
-            checkCallback: (checking: boolean) => {
-                const view = workspace.getActiveViewOfType(MarkdownView);
-                if (view) {
-                    let mathLink = getMathLink();
-                    if (mathLink != undefined) {
-                        if (typeof mathLink[0] === 'string') {
-                            if (!checking) {
-                                // Change to edit mode
-                                let currentState = workspace.getLeaf().getViewState();
-                                currentState.state.mode = 'source';
-                                workspace.getLeaf(false).setViewState(currentState);
-
-                                // Select mathLink to edit
-                                view.editor.setSelection({line: mathLink[1], ch: 10}, {line: mathLink[1], ch: view.editor.getLine(mathLink[1])});
-                                view.editor.focus();
-                            }
-                            return true;
-                        }                       
-                    }
-                }
-                return false;
-            }
-        });
-
-        // Get frontMatter as string[] and EditorRange. Undefined otherwise.
-        function getFrontMatter(): [string[], EditorRange] | undefined {
-            const editor = workspace.getActiveViewOfType(MarkdownView).editor;
-            if (editor.getLine(0) === '---') {
-                let lineNumber = 1;
-                let lineCount = editor.lineCount();
-                let lines: string[] = ['---'];
-                while (true) {
-                    let line = editor.getLine(lineNumber);
-                    lines.push(line);
-                    if (line === '---') {
-                        return [lines, {from: {line: 0, col: 0}, to: {line: lineNumber, col: 3}}];
-                    } else if (lineNumber > lineCount ) {
-                        return undefined;
-                    } else {
-                        lineNumber++;
-                    }
-                }
-            }
-            return undefined;
-        }
 
         // Get mathLink as string (with lineNumber). If key exists but not value, return null (with lineNumber). Undefined otherwise.
-        function getMathLink(): [string, number] | [null, number] | undefined {
-            const editor = workspace.getActiveViewOfType(MarkdownView).editor;
-            if (editor.getLine(0) === '---') {
-                let lineNumber = 1;
-                while (true) {
-                    let line = editor.getLine(lineNumber);
-                    let key = line.substring(0, 10);
-                    let value = line.replace(key, '');
-                    if (key === 'mathLink: ') {
-                        if (value != '')
-                            return [value, lineNumber];
-                        else
-                            return [null, lineNumber];
-                    } else if (line === '---') {
-                        return undefined;
-                    } else {
+        async function getMathLink(file?: TFile): [string, number] | [null, number] | undefined {
+            let contents = [];
+            if (file != null) {
+                contents = await vault.read(file);
+                contents = contents.split(/\r?\n/);
+            } else {
+                const editor = workspace.getActiveViewOfType(MarkdownView).editor;
+                for (let i = 0; i < editor.lineCount(); i++) {
+                    contents.push(editor.getLine(i));
+                }
+            }
+
+            if (contents[0] === '---') {
+                for (let lineNumber = 1; lineNumber < contents.length; lineNumber++) {
+                    let line = contents[lineNumber];
+                    if (line.length < 10) {
                         lineNumber++;
+                    } else {
+                        let key = line.substring(0, 10);
+                        if (key === 'mathLink: ') {
+                            let value = line.replace(key, '');
+                            if (value != '')
+                                return [value, lineNumber];
+                            else
+                                return [null, lineNumber];
+                        } else if (line === '---') {
+                            return undefined;
+                        } else {
+                            lineNumber++;
+                        }
                     }
                 }
             }
             return undefined;
-        }
-
-        // Runs when file is change
-        // Ideally, I want to run it when a link is added/new mathLink is created
-        // Will need to look into it...
-        metadataCache.on('changed', (file: TFile, data: string, cache: CachedMetaData) => {
-            // Get (out)links; run if non-empty
-            let links = cache.links;
-            if (Array.isArray(links)) {
-                // Loop through all links
-                links.forEach((link) => {
-                    // Get path of file
-                    let linkPath = fileManager.getNewFileParent(link.link).path;
-
-                    // Get file
-                    let linkFile;
-                    if (isDisplayLink(link))
-                        linkFile = vault.getAbstractFileByPath(linkPath + '/' + link.link);
-                    else
-                        linkFile = vault.getAbstractFileByPath(linkPath + '/' + link.link + '.md');
-
-                    // If it is a file
-                    if (linkFile instanceof TFile) {
-                        // Get frontMatter
-                        let frontMatter = metadataCache.getFileCache(linkFile).frontmatter;
-                        if (frontMatter != undefined) {
-                            // Get mathLink
-                            let mathLink = frontMatter.mathLink;
-                            if (mathLink != undefined && mathLink != null) {
-                                let startPos = {line: link.position.start.line, ch: link.position.start.col};
-                                let endPos = {line: link.position.end.line, ch: link.position.end.col};
-                                // Update if already exists; new otherwise
-                                if (isDisplayLink(link)) {
-                                    let current = link.original.replace(/\[/, '')
-                                        .replace(/\]\(([^\$^\[^\]]+%20)+[^\$^\[^\]]*(\.md)*\)/, '');
-                                    if (current != mathLink) {
-                                        let leftOver = link.original.replace(current, '').replace('[]', '');
-                                        updateMathLink(mathLink, startPos, endPos, leftOver);
-                                    }
-                                } else {
-                                    let obsidianFormatted = link.link.replace(/^/, '(').replace(/$/, '.md)').replace(/\s/g, '%20');
-                                    newMathLink(mathLink, startPos, endPos, obsidianFormatted);
-                                }
-                            }
-                        }
-                    }
-                });
-            }
-
-            console.log('Backlinks');
-            let resolvedLinks = metadataCache.resolvedLinks;
-            let keys = Object.keys(resolvedLinks);
-            keys.forEach((key) => {
-                let links = resolvedLinks[key];
-                Object.keys(links).forEach((link) => {
-                    if (link === file.path) {
-                        console.log(key);
-                        console.log(metadataCache.getFirstLinkpathDest(key, key));
-                    }
-                });
-            });
-        });
-
-        function updateMathLink(mathLink: string, startPos: EditorPosition, endPos: EditorPosition, leftOver: string): void {
-            const view = workspace.getActiveViewOfType(MarkdownView);
-            startPos.ch++;
-            endPos.ch = endPos.ch - leftOver.length - 1;
-            view.editor.replaceRange(mathLink, startPos, endPos);
-        }
-
-        function newMathLink(mathLink: string, startPos: EditorPosition, endPos: EditorPosition, obsidianFormatted: string): void {
-            const view = workspace.getActiveViewOfType(MarkdownView);
-            let replaceBy = `[${mathLink}]${obsidianFormatted}`
-            view.editor.replaceRange(replaceBy, startPos, endPos);
-        }
-
-        function isDisplayLink(link: LinkCache): boolean {
-            return link.displayText === "";
         }
     }
 
