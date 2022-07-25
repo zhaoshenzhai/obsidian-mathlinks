c3d7cc03db3d0c609daa87ca1f98a5f31669beb5
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-07-25 15:08:18 +0400

    New mathLinks when a link is created

--- main.ts
+++ main.ts
@@ -5,13 +5,15 @@ export default class MathLinks extends Plugin {
         const { vault } = this.app;
         const { workspace } = this.app;
         const { metadataCache } = this.app;
+        const { fileManager} = this.app;
 
         // Update all links in backLinkFiles if file (with mathLink) is changed.
-        // Want to modify it so it runs only if a mathLink is updated/generated.
+        //     Want to modify it so it runs only if a mathLink is updated/generated.
+        // Generate mathLinks of outLinks in file if file  is change.
+        //     Want to midify it so it runs only if a link is created.
         metadataCache.on('changed', async (file: TFile, data: string, cache: CachedMetaData) => {
-            console.log(file.name);
             let mathLink = await getMathLink(file);
-            if (mathLink != null) {
+            if (mathLink != null && mathLink != undefined) {
                 let backLinkFilePaths: string[] = [];
                 Object.keys(metadataCache.resolvedLinks).forEach((key) => {
                     let links = metadataCache.resolvedLinks[key];
@@ -34,7 +36,7 @@ export default class MathLinks extends Plugin {
 
                             let rightFormatted = right.replace(/\./, '\\.').replace(/\(/, '\\(').replace(/\)/, '\\)').replace(/\%/, '\\%');
                             let mixedLink = new RegExp('\\[((?!\\]\\(|\\]\\]).)*\\]' + rightFormatted, 'g');
-                            let doubleLink = new RegExp(file.name.replace(/^/, '\\[\\[').replace(/\.md$/, '\\]\\]').replace(/\s/, '\\s'), 'g');
+                            let doubleLink = new RegExp(file.name.replace(/^/, '\\[\\[').replace(/\.md$/, '\\]\\]').replace(/\s/g, '\\s'), 'g');
 
                             let modified = backLinkFileContent.replace(mixedLink, newLink).replace(doubleLink, newLink);
                             if (backLinkFileContent != modified) {
@@ -44,6 +46,32 @@ export default class MathLinks extends Plugin {
                     });
                 }
             }
+
+            let fileContent = await vault.read(file);
+            let modified = fileContent;
+            cache.links.forEach(async (outLink) => {
+                let outLinkFileName = outLink.link;
+                if (outLink.displayText != "")
+                    outLinkFileName = outLinkFileName.replace(/$/, '.md');
+                let outLinkFilePath = fileManager.getNewFileParent(outLinkFileName).path + '/' + outLinkFileName;
+                let outLinkFile = vault.getAbstractFileByPath(outLinkFilePath);
+
+                if (outLinkFile instanceof TFile) {
+                    let outLinkMathLink = await getMathLink(outLinkFile);
+                    if (outLinkMathLink != null && outLinkMathLink != undefined) {
+                        let doubleLink = new RegExp(outLinkFileName.replace(/^/, '\\[\\[').replace(/\.md$/, '\\]\\]').replace(/\s/g, '\\s'), 'g');
+
+                        let left = outLinkMathLink[0].replace(/^/, '[').replace(/$/, ']');
+                        let right = outLinkFileName.replace(/^/, '(').replace(/$/, ')').replace(/\s/g, '%20');
+                        let newLink = `${left}${right}`;
+
+                        modified = modified.replace(doubleLink, newLink);
+                        if (fileContent != modified) {
+                            await vault.modify(file, modified);
+                        }
+                    }
+                }
+            });
         });
 
         // Get mathLink as string (with lineNumber). If key exists but not value, return null (with lineNumber). Undefined otherwise.
