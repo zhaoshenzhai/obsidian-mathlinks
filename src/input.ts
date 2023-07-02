f1ff04635a14a96a1d55e823cd6dd9d397819fef
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2023-07-02 16:00:50 -0400

    fix: open note in new tab when `mouse-wheel-button` is held

--- src/input.ts
+++ src/input.ts
@@ -1,8 +1,10 @@
 export class Input {
     ctrlKey: boolean = false;
+    mouseMiddle: boolean = false;
 
     static init() {
         this.addCtrlListener(document);
+        this.addMouseMiddleListener(document);
     }
 
     static addCtrlListener(element: HTMLElement) {
@@ -15,6 +17,20 @@ export class Input {
         element.addEventListener("keyup", (evt: KeyboardEvent) => {
             if (evt.key == "Control") {
                 this.ctrlKey = false;
+            }
+        });
+    }
+
+    static addMouseMiddleListener(element: HTMLElement) {
+        element.addEventListener("mousedown", (evt: MouseEvent) => {
+            if (evt.button == 1) {
+                this.mouseMiddle = true;
+            }
+        });
+
+        element.addEventListener("mouseup", (evt: MouseEvent) => {
+            if (evt.button == 1) {
+                this.mouseMiddle = false;
             }
         });
     }
