19ade6bfaedb4338a0188ece511885cf426439a1
RyotaUshio <72342591+RyotaUshio@users.noreply.github.com>
2023-07-28 21:12:37 +0900

    avoid obtaining the same cache twice

--- src/tools.ts
+++ src/tools.ts
@@ -113,7 +113,7 @@ export function getMathLink(plugin: MathLinks, linktext: string): string {
     if (cache.frontmatter) {
         if (subpathResult) {
             if (subpathResult.type == 'heading') { 
-                mathLink = getMathLink(plugin, path) + ' > ' + subpathResult.current.heading;
+                mathLink = cache.frontmatter.mathLink + ' > ' + subpathResult.current.heading;
             } else if (subpathResult.type == 'block' && cache.frontmatter["mathLinks-block"]) {
                 mathLink = cache.frontmatter["mathLinks-block"][subpathResult.block.id];
             }
