bfc8b76baec5240408552a662fd87605a6a577cd
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-08-07 16:27:10 +0900

    add files needed for publishing the API as a npm package

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export * from "./api";
+
+// Implementation
+
+import type { App, Plugin } from "obsidian";
+import { MathLinksAPIAccount } from "./api";
+
+// Utility functions
+
+/**
+ * When called for the first time, register userPlugin as a user of MathLinks API and returns its account.
+ * From the second time on, returns the existing account of userPlugin.
+ */
+export const getAPIAccount = (userPlugin: Plugin): MathLinksAPIAccount | undefined => {
+    return userPlugin.app.plugins.plugins.mathlinks?.getAPIAccount(userPlugin);
+};
+
+/** Check if MathLinks is enabled. */
+export const isPluginEnabled = (app: App) => app.plugins.enabledPlugins.has("mathlinks");
